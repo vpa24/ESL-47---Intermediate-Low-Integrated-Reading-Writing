0a2f677cafd36e3289a04d2bd27d15dfe87774d5
Phuong Anh Vu <vuphuonganh020497@gmail.com>
2023-11-19 21:55:28 -0800

    Update config logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,6 +27,9 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+# logo
+html_logo = "_static/logo.png"
+
 # -- Options for HTML output
 
 html_theme = 'pydata_sphinx_theme'
